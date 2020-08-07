e99a41478a5fbf8fd12b1924ac8d35cd36b9dbc0
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-08-07 12:05:11 -0400

    Finished up large portion of the main function in cards js file

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,29 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+function createCard() {
+    const card = document.createElement('div')
+    const cardHeadline = document.createElement('div')
+    const cardAuthor = document.createElement('div')
+    const cardImgContainer = document.createElement('div')
+    const cardImg = document.createElement('img')
+    const cardName = document.createElement('span')
+
+    card.appendChild(cardHeadline)
+    card.appendChild(cardAuthor)
+    cardAuthor.appendChild(cardImgContainer)
+    cardImgContainer.appendChild(cardImg)
+    cardAuthor.appendChild(cardName)
+
+    card.classList.add('card')
+    cardHeadline.classList.add('headline')
+    cardAuthor.classList.add('author')
+    cardImgContainer.classList.add('img-container')
+    
+    //cardHeadline.textContent = ;
+    //cardImg.src = ;
+    //cardAuthor.textContent = `By: ${}`;
+
+    return card;
+}
